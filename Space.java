a7255403685b5805b378b30be13ff3d8682cf610
Malleshwari M <malleshwarimayilsamy@gmail.com>
2025-01-25 16:05:49 +0530

    Create Space.java

---
+++ Space.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Space {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        String b = in.next(); //If i give in.nextLine() means it show a input mismatch error
+        float c = in.nextFloat();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
